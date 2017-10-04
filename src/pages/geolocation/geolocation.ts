9010fe37364a42aafbea67dfb2e9bf4a35954fbc
Rosber Almeida Junior <rosber.almeida@autotrac.corp>
2017-10-04 07:39:07 -0300

    ter

--- src/pages/geolocation/geolocation.ts
+++ src/pages/geolocation/geolocation.ts
@@ -16,11 +16,14 @@ import { Geolocation,Geoposition } from '@ionic-native/geolocation';
 export class GeolocationPage {
 
  geoResults: Geoposition;
-  constructor(public navCtrl: NavController,
+ timestampConvertido: any;
+
+ constructor(public navCtrl: NavController,
      public navParams: NavParams,
      private geolocation:Geolocation
     ) {
-  }
+
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GeolocationPage');
@@ -30,35 +33,13 @@ export class GeolocationPage {
 getGeolocation(){
   this.geolocation.getCurrentPosition().then((resp) => {
   this.geoResults=resp;
-
+  this.timestampConvertido=new Date(this.geoResults.timestamp)
    }).catch((error) => {
      console.log('Error getting location', error);
    });
 
 
 }
-//YYYY-MM-DDTHH:mmZ
-convertTimestamp2ISO8602(timestamp:number)
-{
- let ano:string='',isoString: string='';
-let data=new Date(timestamp)
-isoString=data.toString();
-
-return isoString;
-}
-
-convertTimestamp2Date(timestamp:number)
-{
- let ano:string='',isoString: string='';
-let data=new Date(timestamp)
-isoString=data.toString();
-
-return data;
-}
-
-
-
-
 
 }
 
